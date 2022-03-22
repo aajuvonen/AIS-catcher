88eac969693ee84bb18987c49536b1573e3af9ce
Artturi Juvonen <artturi@juvonen.eu>
2022-03-22 18:43:05 +0200

    typofix on frequency

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -465,7 +465,7 @@ int main(int argc, char* argv[])
 
 		// override sample rate if defined by user
 		if (sample_rate) device->setSampleRate(sample_rate);
-		device->setFrequency((int)(433e8));
+		device->setFrequency((int)(4338e5));
 
 		// ------------
 		// Setup models
